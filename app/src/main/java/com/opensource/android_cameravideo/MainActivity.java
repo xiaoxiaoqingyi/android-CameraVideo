3de5128bbb1357eb37be0da4f774e788de6ae0dd
xiaoxiaoqingyi <lideyi333@163.com>
2017-06-13 14:15:05 +0800

    update

--- app/src/main/java/com/opensource/android_cameravideo/MainActivity.java
+++ app/src/main/java/com/opensource/android_cameravideo/MainActivity.java
@@ -3,11 +3,15 @@ package com.opensource.android_cameravideo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import okio.Okio;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
